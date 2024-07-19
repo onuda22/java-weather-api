c0b4510d4925b74fc657770d0fcaa7971f0678cd
vigorjs <111201912289@mhs.dinus.ac.id>
2024-07-19 17:55:10 +0700

    fix: db redundant when fetch data from api

--- src/main/java/enigma/weather/service/implementation/WeatherServiceImpl.java
+++ src/main/java/enigma/weather/service/implementation/WeatherServiceImpl.java
@@ -15,6 +15,7 @@ import enigma.weather.util.dto.WeatherResponseDTO;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WeatherServiceImpl implements WeatherService {
@@ -46,13 +47,41 @@ public class WeatherServiceImpl implements WeatherService {
                     .body(WeatherResponseDTO.class);
 
             Weather result = ConvertToWeather.convertToWeather(dto);
-
-            weatherRepository.save(result);
+            updateWeatherData(result);
 
             return dto;
 
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void updateWeatherData(Weather weather) {
+        Optional<Weather> existingWeather = weatherRepository.findByLatitudeAndLongitudeAndUtcOffsetSeconds(
+                weather.getLatitude(), weather.getLongitude(), weather.getUtcOffsetSeconds());
+
+        if (existingWeather.isPresent()) {
+            Weather weatherToUpdate = existingWeather.get();
+            List<Daily> existingDailyData = weatherToUpdate.getDaily();
+            List<Daily> newDailyData = weather.getDaily();
+
+            // Update existing weather fields
+            weatherToUpdate.setGenerationtimeMs(weather.getGenerationtimeMs());
+            weatherToUpdate.setTimezone(weather.getTimezone());
+            weatherToUpdate.setTimezone_abbreviation(weather.getTimezone_abbreviation());
+            weatherToUpdate.setElevation(weather.getElevation());
+            weatherToUpdate.setDailyUnits(weather.getDailyUnits());
+
+            // Add new daily entries
+            for (Daily newDaily : newDailyData) {
+                if (existingDailyData.stream().noneMatch(d -> d.getTime().equals(newDaily.getTime()))) {
+                    newDaily.setWeather(weatherToUpdate);
+                    existingDailyData.add(newDaily);
+                }
+            }
+            weatherRepository.save(weatherToUpdate);
+        } else {
+            weatherRepository.save(weather);
         }
     }
 }
